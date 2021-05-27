d4834c8c0100d730e01a21d2cd46b266bdd031e9
Pradeep <mgpradeepmg@gmail.com>
2021-05-27 20:50:53 +0530

    my first projet

---
+++ Learningcsharpmay26/Csharpfirstproject/Basics.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Learningcsharpmay26.Csharpfirstproject
+{
+    public class Basics
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("hello pradeep");
+            
+            string str;
+            str = " pradeep ";
+            Console.WriteLine(str.Length);
+            str = str.Trim();   
+            Console.WriteLine(str.Length);
+            Console.WriteLine($"this value is {str}");
+        }
+    }
+}
---
+++ Learningcsharpmay26/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace Learningcsharpmay26
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
